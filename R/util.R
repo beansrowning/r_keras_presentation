6110cec7ac1326339bde132e527d43691fab55f4
beansrowning <sean.browning94@gmail.com>
2019-09-19 12:41:42 -0400

    First commit

---
+++ R/util.R
@@ -0,0 +1,112 @@
+# Helper Functions
+# Sean Browning
+# ====================
+
+#' @title Sample points on a larger sphere enclosing a smaller sphere
+#' @description
+#' Dataset generation function to produce data points for two enclosed spheres
+#' a simple toy dataset for classification in 3 dimensions. A logical extension
+#' to the \code{make_circles()} function within SK-learn.
+#'
+#' @param samples (int) The total number of points generated.
+#' @param shuffle (bool) Whether to shuffle the samples.
+#' @param noise (numeric) Standard deviation of Gaussian noise added to the data.
+#' @param state (int) Seed for RNG scope
+#' @param factor (numeric) Scale factor between inner and outer circle (<1).
+
+#' @return data.frame of dim [samples, 4] containing x, y, and z coordinates along with an indicator column for outer and inner values
+#' @importFrom stats rnorm runif
+
+#' @export
+make_spheres <- function(samples = 1000L, shuffle = TRUE, noise = NULL, state = NULL, factor = 0.8) {
+  
+  if (! is.null(state)) {
+    set.seed(state)
+  }
+  
+  # Sample phi and theta from a uniform distribution [0 to 2pi]
+  phi <- runif(n = floor(samples / 2), min = 0, max = 2 * pi)
+  theta <- runif(n = floor(samples / 2), min = 0, max = 2 * pi)
+  
+  # Could also use Marsaglia (1972) method for sampling points on the surface of
+  # an n-ball, where x1, x2, ..., xn are sampled from independent uniform distributions
+  # (-1, 1), and rejecting any points such that x1 + x2 + ... + xn >= 1.
+  # and using the following equations:
+  # x	=	2 * x1 * sqrt(1 - x1^2 - x2^2)
+  # y	=	2 * x2 * sqrt(1 - x1^2 - x2^2)
+  # z	=	1 - 2(x1^2 + x2^2)
+  
+  outer <- data.frame(
+    x = sqrt(1 - (cos(phi) ^ 2)) * cos(theta),
+    y = sqrt(1 - (cos(phi) ^ 2)) * sin(theta),
+    z = cos(phi)
+  )
+  
+  # Create inner points by multiplying outer by a scale factor
+  inner <- outer * factor
+  
+  out <- rbind(outer, inner)
+  
+  out$id <- rep(c("outer", "inner"), each = floor(samples / 2))
+  
+  
+  if (! is.null(noise)) {
+    out[["x"]] <- out[["x"]] + rnorm(n = samples, sd = noise)
+    out[["y"]] <- out[["y"]] + rnorm(n = samples, sd = noise)
+    out[["z"]] <- out[["z"]] + rnorm(n = samples, sd = noise)
+  }
+  
+  if (shuffle) {
+    out <- out[sample(seq(samples), samples, replace = FALSE), ]
+  }
+  
+  return(out)
+}
+
+
+#' @title SK-learn.datasets make_circles() implemented in R
+#' @description
+#' Make a large circle containing a smaller circle in 2d.
+#' A simple toy dataset to visualize clustering and classification algorithms.
+#' Originally implemented in Scikit-learn.
+#'
+#' @param samples (int) The total number of points generated.
+#' @param shuffle (bool) Whether to shuffle the samples.
+#' @param noise (numeric) Standard deviation of Gaussian noise added to the data.
+#' @param state (int) Seed for RNG scope
+#' @param factor (numeric) Scale factor between inner and outer circle (<1).
+
+#' @return data.frame of dim [samples, 3] containing x and y coordinates along with an indicator column for outer and inner values
+#' @importFrom stats rnorm
+
+#' @export
+make_circles <- function(samples = 100L, shuffle = TRUE, noise = NULL, state = NULL, factor = 0.8) {
+  
+  if (! is.null(state)) {
+    set.seed(state)
+  }
+  
+  points <- seq(0, 2 * pi, length.out = floor(samples / 2) + 1L)[-1]
+  
+  outer_circ_x <- cos(points)
+  outer_circ_y <- sin(points)
+  inner_circ_x <- outer_circ_x * factor
+  inner_circ_y <- outer_circ_y * factor
+  
+  out <- data.frame(
+    x = c(outer_circ_x, inner_circ_x),
+    y = c(outer_circ_y, inner_circ_y),
+    circ = rep(c("outer", "inner"), each = floor(samples / 2))
+  )
+  
+  if (! is.null(noise)) {
+    out[["x"]] <- out[["x"]] + rnorm(n = samples, sd = noise)
+    out[["y"]] <- out[["y"]] + rnorm(n = samples, sd = noise)
+  }
+  
+  if (shuffle) {
+    out <- out[sample(seq(samples), samples, replace = FALSE), ]
+  }
+  
+  return(out)
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Deep Learning and R
+## September 26th, 2019 
+*Sean Browning*  
+
+## What are we doing here?  
+
+I've used Tensorflow and Keras both for one-off projects and production applications alike over the past year or so.
+
+I want to share some applied deep learning examples in R as a primer for others who may want to use these tools. 
+
+In opposition to the usual 10,000ft view of deep learning, I hope to show some tangible examples that demonstrate what deep learning can (and cannot) do. 
+
+## How can I build it locally?  
+
+From R or RStudio render the RMarkdown document and bingo-boingo.  
+
+```r
+rmarkdown::render("Rmd/presentation.Rmd")
+```  
+
+## How can I save this to PDF?  
+
+The best way would be to print the HTML document to a PDF using google chrome.  
+
+Simply Ctrl+P when displaying the slides in Chrome, and change the destination as `Save as PDF`  
+
+## I want to help and make changes!  
+
+Thanks for helping out!
+
+Feel free to make a fork of this repository and submit a PR and I'll review it.  
